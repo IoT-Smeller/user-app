8c10ae9226b48a50ec4c8616c7ad6bd28ef3b670
jadenngo13 <jadenngo13@gmail.com>
2023-02-02 10:01:22 -0800

    Initial Commit

---
+++ userApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  userApp
+//
+//  Created by Colette Bradley on 2/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ userApp/userAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  userAppApp.swift
+//  userApp
+//
+//  Created by Colette Bradley on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct userAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
